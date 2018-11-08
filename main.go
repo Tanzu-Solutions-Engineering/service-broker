dd0b2a2d8aecb43d4b68073acaecb7a58cbd4d30
reagul <reagul.2007@gmail.com>
2018-11-08 14:33:20 -0500

    initial commit

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
@@ -16,14 +15,20 @@ func main() {
 	workshopBroker := broker.Broker{}
 
 	username := os.Getenv("SECURITY_USER_NAME")
+	if username == "" {
+		logger.Debug("SECURITY_USER_NAME is not set! Defaulting to \"pivotal\"")
+		username = "pivotal"
+	}
 	password := os.Getenv("SECURITY_USER_PASSWORD")
-
-	if len(username) == 0 {
-		logger.Fatal("SECURITY_USER_NAME is not set!", fmt.Errorf("SECURITY_USER_NAME is not set!"))
+	if password == "" {
+		logger.Debug("SECURITY_USER_PASSWORD is not set! Defaulting to \"pivotal\"")
+		password = "pivotal"
 	}
 
-	if len(password) == 0 {
-		logger.Fatal("SECURITY_USER_PASSWORD is not set!", fmt.Errorf("SECURITY_USER_PASSWORD is not set!"))
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		logger.Debug("PORT is not set! Defaulting to \"8080\"")
+		port = "8080"
 	}
 
 	brokerCreds := brokerapi.BrokerCredentials{
@@ -33,10 +38,6 @@ func main() {
 
 	handler := brokerapi.New(workshopBroker, logger, brokerCreds)
 
-	port := os.Getenv("PORT")
-	if len(port) == 0 {
-		logger.Fatal("PORT is not set!", fmt.Errorf("PORT is not set"))
-	}
 	err := http.ListenAndServe(":"+port, handler)
 	logger.Fatal("HTTP Service Failed", err, lager.Data{})
 }
